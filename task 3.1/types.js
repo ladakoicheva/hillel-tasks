88e511b776648403742e5d474b16220e12a346e6
Lada Koicheva <koichevalada@gmail.com>
2025-07-22 19:22:10 +0300

    task 3.1 is done

---
+++ task 3.1/types.js
@@ -0,0 +1,11 @@
+
+console.log(typeof (5));
+console.log(typeof (57856587657877578456n));
+console.log(typeof ("I learned datatypes"));
+console.log(typeof (true));
+console.log(typeof (undefined));
+console.log(typeof (null));
+console.log(typeof (function(){}));
+
+
+
