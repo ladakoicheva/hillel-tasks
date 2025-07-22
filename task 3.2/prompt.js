8c711a33d275e080bb01e22a3dc9e143c6419881
Lada Koicheva <koichevalada@gmail.com>
2025-07-22 19:51:50 +0300

    task 3.2 is done

---
+++ task 3.2/prompt.js
@@ -0,0 +1,6 @@
+const str1 = prompt("Введіть перший рядок");
+const str2 = prompt("Введіть другий рядок");
+const str3 = prompt("Введіть третій рядок");
+console.log(`3: ${str3}, 1: ${str1}, 2: ${str2}`);
+
+
