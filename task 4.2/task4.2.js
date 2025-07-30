95436f8391784f1a311dd67de1bef4db386647cf
Lada Koicheva <koichevalada@gmail.com>
2025-07-30 17:16:06 +0300

    edited 4.2

--- task 4.2/task4.2.js
+++ task 4.2/task4.2.js
@@ -1,11 +1,15 @@
-let number = prompt("Введіть тризначне число:")
- isNaN(number) || number < 100 || number > 999
-  ? console.log("Це не тризначне число")
-  : number[0] === number[1] && number[1] === number[2]
-  ? console.log("Всі цифри однакові")
-  : number[0] === number[1] ||
-    number[1] === number[2] ||
-    number[0] === number[2]
-  ? console.log("Серед цифр є однакові.")
-  : console.log("Всі цифри різні.");
-        
+let number = prompt("Введіть тризначне число:");
+
+if (isNaN(number) || number < 100 || number > 999) {
+  console.log("Це не тризначне число");
+} else if (number[0] === number[1] && number[1] === number[2]) {
+  console.log("Всі цифри однакові");
+} else if (
+  number[0] === number[1] ||
+  number[1] === number[2] ||
+  number[0] === number[2]
+) {
+  console.log("Серед цифр є однакові.");
+} else {
+  console.log("Всі цифри різні.");
+}
