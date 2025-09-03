75b93fdf6ff0e4d371bc91729e7b76f1d34fdb97
Lada Koicheva <koichevalada@gmail.com>
2025-09-04 01:22:59 +0300

    edited task 11.2

--- task 11.2/task11.2.js
+++ task 11.2/task11.2.js
@@ -1,16 +1,6 @@
 const text = document.querySelector(".text");
 const button = document.querySelector(".button");
-let isColored = false;
-const originalColor = getComputedStyle(text).color;
 
 button.addEventListener("click", () => {
-  if (!isColored) {
-    isColored = true;
-    text.style.color = "#ff0055";
-    text.style.textShadow = "0 0 5px #ff0055, 0 0 10px #ff0055";
-  } else {
-    text.style.color = originalColor;
-    text.style.textShadow = "none";
-    isColored = false;
-  }
+  text.classList.toggle("colored");
 });
