22e9b2e726341a04451f632d248aef74ed6c955c
Lada Koicheva <koichevalada@gmail.com>
2025-09-10 12:55:57 +0300

    feat: store tasks in array and render dynamically

--- task 12.3/task.12.3.js
+++ task 12.3/task.12.3.js
@@ -1,22 +1,31 @@
 const taskList = document.querySelector(".list");
 const taskInput = document.querySelector(".task");
 const addTaskBtn = document.querySelector(".add");
+tasks = [];
 
+function renderTasks() {
+  taskList.innerHTML = "";
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${task} <button class="delete-btn" data-index="${index}">X</button>`;
+    taskList.appendChild(li);
+  });
+}
 function addTask() {
   const taskText = taskInput.value.trim();
   if (taskText === "") return;
 
-  const li = document.createElement("li");
-  li.innerHTML = `${taskText} <button class="delete-btn">X</button>`;
-  taskList.appendChild(li);
+  tasks.push(taskText); 
+  renderTasks(); 
   taskInput.value = "";
 }
 
 addTaskBtn.addEventListener("click", addTask);
 
-
-taskList.addEventListener('click', (todo) => {
-    if (todo.target.tagName === "BUTTON") {
-      todo.target.parentElement.remove();
-    }
-})
+taskList.addEventListener("click", (todo) => {
+  if (todo.target.classList.contains("delete-btn")) {
+       const index = todo.target.dataset.index;
+       tasks.splice(index, 1); 
+       renderTasks();  
+  }
+});
